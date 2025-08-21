7dacfbf1ae4a880b7f78d105edd1b07868ccc54d
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-21 15:34:06 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/sylviavitoria/automacaosistemafaculdade/AutomacaosistemafaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.automacaosistemafaculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaosistemafaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaosistemafaculdadeApplication.class, args);
+	}
+
+}
